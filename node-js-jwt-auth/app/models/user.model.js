48475eb277000c28fe9b1610c2e912ca77c977a7
evan baillieu <evanbaillieu@pc231.home>
2021-03-15 20:12:17 +0100

    add the node api and jwt connect for my api

---
+++ node-js-jwt-auth/app/models/user.model.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, Sequelize) => {
+  const User = sequelize.define("users", {
+    descri{
+      type: Sequelize.STRING,
+    },
+    nom: {
+      type: Sequelize.STRING
+    },
+    prenom: {
+      type: Sequelize.STRING
+    },
+    login: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.STRING
+    }
+  });
+
+  return User;
+};
